0bafa03aa0d4e53988c2e85b38acfe817ada2635
moskib <30794120+moskib@users.noreply.github.com>
2018-07-22 17:32:33 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
+// Modules:
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+
+
+// Components:
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { TopicsComponent } from './topics/topics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    AuthorsComponent,
+    TopicsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'authors',
+        component: AuthorsComponent
+      },
+      {
+        path: 'topics',
+        component: TopicsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
